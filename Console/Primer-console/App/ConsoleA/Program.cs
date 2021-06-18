7c471c52fd93b2da17285758465bd5413f37d17d
PILLAJO MONTACHANA KEVIN HERNAN <52302923+khpm-1roa@users.noreply.github.com>
2021-06-18 17:26:09 -0500

    Add files via upload

---
+++ Console/Primer-console/App/ConsoleA/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args != null && args.Length > 1)
+            {
+                Console.WriteLine($"Hay {args.Length} argumentos");
+                Console.WriteLine($"Hola tu nombre es: {args[0]}");
+                if(args.Length > 1)
+                Console.WriteLine($"Hola tu apellido es: {args[1]}");
+            }
+            else
+                Console.WriteLine("No hay argumentos");
+            Console.Read();
+        }
+    }
+}
