81b321478f14dd0fc4c6b28ea94fae59c5550acd
PILLAJO MONTACHANA KEVIN HERNAN <52302923+khpm-1roa@users.noreply.github.com>
2021-07-01 19:02:03 -0500

    Add files via upload

---
+++ Inyecciones-master/DatosOtro/Persistencia2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Interfaces;
+using Entidades;
+namespace DatosOtro
+{
+    class Persistencia2 :IPersistencia
+    {
+        public bool Grabar(Estudiante estudiante)
+        {
+            // to do 
+            // creacion de archivos 
+            File.AppendAllLines("Estudiante.txt", new List<string> { estudiante.Nombre});
+            // creacion de archivos 
+            return true;
+        }
+        public bool Grabar(Curso curso)
+        {
+            // to do 
+            // creacion de archivos 
+            File.AppendAllLines("Curso.txt", new List<string> { curso.Nombre});
+            // creacion de archivos 
+            return true;
+        }
+        public bool Grabar(EstudiantePorCurso estudiantePorCurso)
+        {
+            // to do 
+            // creacion de archivos 
+            File.AppendAllLines("EstudiantePorCurso.txt", new List<string> { estudiantePorCurso.CursoId.ToString()});
+            // creacion de archivos 
+            return true;
+        }
+    }
+   
+    }
+}
